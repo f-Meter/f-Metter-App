6b5435ac1165ab5a66384de74b89e043efbca388
f-Meter <126366977+f-Meter@users.noreply.github.com>
2023-03-15 13:12:43 -0600

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const contenedorQR = document.getElementById('contenedorQR');
-const qr = document.getElementById('formulario');
-
-const QR = new QRCode(contenedorQR);
-
-qr.addEventListener('submit', (e) => {
-	e.preventDefault();
-	QR.makeCode("www.facebook.com");
-});
